26a6beceb3282bd61b7ced5c2f87c7633ba423c7
IssaDia <issadiapro@gmail.com>
2020-04-15 13:20:28 +0200

    Added style to body

---
+++ pages/dashboard.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const Dashboard: React.FC<{}> = () => (
+  
+    <div className="container">
+    <h1>Hello Next.js</h1>
+    <p>Let's explore different ways to style Next.js apps</p>
+    <style jsx>{`
+      .container {
+        margin: 50px;
+      }
+      p {
+        color: blue;
+      }
+    `}</style>
+    <style jsx global>{`
+      body {
+        background: #f7f7f7;
+      }
+    `}</style>
+  </div>
+    );
+ 
+
+  export default Dashboard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Head from 'next/head'
 import Title from '../components/Title';
+import Link from 'next/link';
+
+const linkStyle = {
+    color: 'red'
+  };
 
 const Home: React.FC<{}> = () => (
     <div className="container">
         <Head>
-        <title>Easyblue, assureur 100% digital pour les entrepreneurs</title>
-            <link rel="icon" href="/easyblue.ico"/>
+            <title>Easyblue, assureur 100% digital pour les entrepreneurs</title>
+            <link rel="icon" href="/easyblue.ico" />
         </Head>
 
-        <Title text="Test frontend."/>
+        <Title text="Test frontend." />
+        <Link href="/dashboard">
+            <a style={linkStyle}>Dashboard page</a>
+        </Link>
     </div>
 );
 
