bf61a1ddf6035bf6da2377de162b9b087a5c4141
IssaDia <issadiapro@gmail.com>
2020-04-15 12:25:58 +0200

    Changed title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import Title from '../components/Title';
 const Home: React.FC<{}> = () => (
     <div className="container">
         <Head>
-            <title>Easyblue: test frontend</title>
-            <link rel="icon" href="/favicon.ico"/>
+        <title>Easyblue, assureur 100% digital pour les entrepreneurs</title>
+            <link rel="icon" href="/easyblue.ico"/>
         </Head>
 
         <Title text="Test frontend."/>
